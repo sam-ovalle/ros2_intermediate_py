eada998364314357a269ad2baa8df9417c543e55
sam-ovalle <samovalle878@gmail.com>
2025-05-23 14:13:09 +0000

    created QoS demo package to show QoS parameters and demo warnings due to incompatible QoS

---
+++ qos_tests_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'qos_tests_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='somebody very awesome',
+    maintainer_email='user@user.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'publisher_custom_minimal_qos_exe = qos_tests_pkg.publisher_custom_minimal_qos:main',
+            'subscriber_custom_minimal_qos_exe = qos_tests_pkg.subscriber_custom_minimal_qos:main',          
+        ],
+    },
+)
