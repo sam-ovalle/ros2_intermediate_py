248be43b689c15226057e281a1ff38ff64a5e02c
sam-ovalle <samovalle878@gmail.com>
2025-05-23 13:51:05 +0000

    created multithreading pkg to demo threading issue through a server

---
+++ multithreading_ros2_examples/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'multithreading_ros2_examples'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'multithreading_movement_server = multithreading_ros2_examples.multithreading_movement_server:main',
+        ],
+    },
+)
